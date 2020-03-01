88828560d41fff4a6f4a1b2e61b8ad52f7506dea
jozimar <jqoj6514311>
2020-03-01 01:23:35 -0400

    routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 module.exports = {
     env: {
         browser: true,
-        es6: true
+        es6: true,
     },
     extends: [
-        "plugin:react/recommended",
-        "prettier",
-        "prettier/react",
-        "airbnb"
+        'prettier',
+        'prettier/react',
+        'airbnb',
     ],
     globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    parse: "babel-eslint",
+    parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
+            jsx: true,
         },
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: 'module',
     },
-    plugins: ["react"],
+    plugins: ['react', 'prettier'],
     rules: {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": ["warn", { extension: [".jsx", ".js"] }]
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extension: ['.jsx', '.js'] },
+        ],
+        'import/prefer-default-export': 'off',
     },
-    "import/prefer-default-export": "off"
 };
