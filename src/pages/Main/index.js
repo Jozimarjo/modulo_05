9e85604f88ee944190eb3d6126bbf622a1068cfe
jozimar <jqoj6514311>
2020-03-02 00:04:54 -0400

    estilzando main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 function Main() {
-    return <Title>Hellow MAIN
-    </Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositorios
+            </h1>
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar repositorios" />
+                <SubmitButton disable>
+                    <FaPlus color="#fff" size={14}></FaPlus>
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
 
 export default Main;
